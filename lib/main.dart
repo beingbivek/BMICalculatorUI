70d0137d38e50aad1d7564937bed78e6962106dd
Bivek Thapa <beingbivek@gmail.com>
2021-04-22 09:55:45 +0545

    learned copywith property of themedata

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,10 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-          primaryColor: Color(0XFF0A0E21),
-          scaffoldBackgroundColor: Color(0xFF0A0E21),
-          accentColor: Colors.amber,
-          textTheme: TextTheme(
-              bodyText2: TextStyle(
-            color: Colors.white,
-          ))),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0XFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
       home: InputPage(),
     );
   }
